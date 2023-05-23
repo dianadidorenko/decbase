5258479e7848dd2aee488497fe7182a5e833a346
Diana Didorenko <125818555+dianadidorenko@users.noreply.github.com>
2023-05-23 17:56:43 +0300

    Initial commit

---
+++ js/script.js
@@ -0,0 +1,13 @@
+// Service Slider
+
+const swiperService = new Swiper(".swiper", {
+  // Optional parameters
+  direction: "horizontal",
+  loop: true,
+
+  // Navigation arrows
+  navigation: {
+    nextEl: ".swiper-button-next",
+    prevEl: ".swiper-button-prev",
+  },
+});
