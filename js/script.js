a22143e564605822d3ae639ece3f1a80fade6f28
Diana Didorenko <125818555+dianadidorenko@users.noreply.github.com>
2023-05-25 14:47:23 +0300

    Update without animation and adaptive mobile/tablet

--- js/script.js
+++ js/script.js
@@ -1,6 +1,20 @@
-// Service Slider
+// Service Slider Service
 
 const swiperService = new Swiper(".swiper", {
+  // Optional parameters
+  direction: "horizontal",
+  loop: true,
+
+  // Navigation arrows
+  navigation: {
+    nextEl: ".swiper-button-next",
+    prevEl: ".swiper-button-prev",
+  },
+});
+
+// Service Slider Testimonials
+
+const swiperTestimonials = new Swiper(".swiper", {
   // Optional parameters
   direction: "horizontal",
   loop: true,
