359b7d06f176df4fb3d73faa748a5aa7d668ddd1
Diana Didorenko <125818555+dianadidorenko@users.noreply.github.com>
2023-05-26 18:43:46 +0300

    Full adaptive

--- js/script.js
+++ js/script.js
@@ -1,3 +1,36 @@
+// Burger menu
+
+function burgerMenu(selector) {
+  let menu = $(selector);
+  let button = menu.find(".burger-menu__button");
+  let links = menu.find(".burger-menu__link");
+  let overlay = menu.find(".burger-menu__overlay");
+
+  button.on("click", (e) => {
+    e.preventDefault();
+    toggleMenu();
+  });
+
+  links.on("click", () => toggleMenu());
+  overlay.on("click", () => toggleMenu());
+
+  function toggleMenu() {
+    menu.toggleClass("burger-menu_active");
+    if (menu.hasClass("burger-menu_active")) {
+      // calling burger menu
+      $("body").css("overflow", "hidden");
+      $(".burger-header-menu").css("display", "flex");
+      // $(".header__container").css("paddingTop", "120px");
+    } else {
+      // when clicking outside the burger menu
+      $("body").css("overflow", "visible");
+      $(".burger-header-menu").css("display", "none");
+      // $(".header__container").css("paddingTop", "24px");
+    }
+  }
+}
+burgerMenu(".burger-menu");
+
 // Service Slider Service
 
 const swiperService = new Swiper(".swiper", {
